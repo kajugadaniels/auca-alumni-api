05e931d72c3b100dcedf456992dcd6a04e4ad284
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-06-16 13:51:02 +0200

    Add URL route for listing all students

--- base/urls.py
+++ base/urls.py
@@ -6,5 +6,5 @@ from django.conf.urls.static import static
 app_name = 'base'
 
 urlpatterns = [
-    # 
+    path('students/', StudentListView.as_view(), name='student-list'),
 ]  + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

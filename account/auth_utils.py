cb4c5da43c5b0e98b2592359845d6eb2f9b91fac
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-06-16 15:12:51 +0200

    Remove session login from auth_utils due to lack of sessions table

--- account/auth_utils.py
+++ account/auth_utils.py
@@ -1,5 +1,3 @@
-from django.contrib.auth import login
-from django.contrib.auth.models import AnonymousUser
 from rest_framework_simplejwt.tokens import RefreshToken
 
 def get_tokens_for_user(user):
@@ -8,8 +6,3 @@ def get_tokens_for_user(user):
         'refresh': str(refresh),
         'access': str(refresh.access_token),
     }
-
-def perform_session_login(request, user):
-    # Prevent double login on anonymous model fallback
-    if not isinstance(user, AnonymousUser):
-        login(request, user, backend='django.contrib.auth.backends.ModelBackend')

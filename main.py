66863d4ae6ca8545f147908cc35d9a921fad32db
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-06-16 22:36:38 +0200

    New changes

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ Main application entry point: include routers and exception handlers.
 from database import engine, Base
 from fastapi import FastAPI, Request
 from fastapi.responses import JSONResponse
-from routers import students, auth, countries, events
+from routers import students, auth, countries
 from fastapi.exceptions import RequestValidationError, HTTPException as FastAPIHTTPException
 
 # Auto-create tables (or manage migrations externally)
@@ -49,8 +49,8 @@ app.include_router(
     tags=["countries"],
 )
 
-app.include_router(
-    events.router,
-    prefix="/api",
-    tags=["events"],
-)
+# app.include_router(
+#     events.router,
+#     prefix="/api",
+#     tags=["events"],
+# )

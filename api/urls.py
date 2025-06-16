c0f699d794f019cda6c31339761c26cf234db9b3
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-06-16 13:46:40 +0200

    New changes

--- api/urls.py
+++ api/urls.py
@@ -1,11 +1,12 @@
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('base.urls')),
-    path('api/auth/', include('account.urls')),,
+    path('api/auth/', include('account.urls')),
 ]
 
 # Serve static and media files

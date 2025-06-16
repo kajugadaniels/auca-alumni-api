07cdb7337e1680fdf1dd933ee9245e951c3b48ae
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-06-16 15:53:30 +0200

    Remove is_active check from UserJWTAuthentication to match custom users table

--- account/authentication.py
+++ account/authentication.py
@@ -4,9 +4,9 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class UserJWTAuthentication(JWTAuthentication):
     """
-    Authenticates a JWT against the User model, using email from `user_email` claim.
+    Authenticates a JWT against the User model using the `user_email` claim.
     """
-    user_id_claim = 'user_email'  # must match SIMPLE_JWT['USER_ID_CLAIM']
+    user_id_claim = 'user_email'
 
     def get_user(self, validated_token):
         user_email = validated_token.get(self.user_id_claim)
@@ -17,8 +17,5 @@ class UserJWTAuthentication(JWTAuthentication):
             user = User.objects.get(email=user_email)
         except User.DoesNotExist:
             raise AuthenticationFailed('User not found.', code='user_not_found')
-
-        if not user.is_active:
-            raise AuthenticationFailed('User is inactive.', code='user_inactive')
 
         return user

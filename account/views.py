c7a9f010ff23e50bc61a5ba3c531fb2013e67bbe
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-06-16 15:16:36 +0200

    Add VerifyTokenView to validate JWT and return user data

--- account/views.py
+++ account/views.py
@@ -1,8 +1,9 @@
 from account.auth_utils import *
-from rest_framework import status
 from account.serializers import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status, permissions
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
 class LoginUserView(APIView):
     def post(self, request):
@@ -46,3 +47,27 @@ class RegisterUserView(APIView):
             "message": "Registration failed.",
             "errors": serializer.errors
         }, status=status.HTTP_400_BAD_REQUEST)
+
+class VerifyTokenView(APIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get(self, request):
+        user = request.user
+
+        # If using a custom unmanaged User model, make sure it's consistent
+        try:
+            user_data = {
+                "email": user.email,
+                "first_name": user.first_name,
+                "last_name": user.last_name,
+                "phone_number": user.phone_number,
+                "student_id": user.student_id
+            }
+        except Exception:
+            return Response({"message": "Invalid user or token."}, status=status.HTTP_401_UNAUTHORIZED)
+
+        return Response({
+            "message": "Token is valid.",
+            "user": user_data
+        }, status=status.HTTP_200_OK)

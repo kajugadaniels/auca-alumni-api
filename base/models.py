a272097a4ad663dd5a5fba697daaf34075211888
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-06-16 13:49:43 +0200

    Add unmanaged Student model linked to existing students table

--- base/models.py
+++ base/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    id_number = models.IntegerField()
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(null=True)
+    updated_at = models.DateTimeField(null=True)
+
+    class Meta:
+        db_table = 'students'
+        managed = False  # Prevent Django from managing the table

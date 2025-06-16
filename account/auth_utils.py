e36deae418ab5d60fcb310d8308826b57fead50b
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-06-16 16:20:19 +0200

    Manually generate JWT tokens for custom User model without relying on for_user or ORM tracking

--- account/auth_utils.py
+++ account/auth_utils.py
@@ -1,8 +1,30 @@
-from rest_framework_simplejwt.tokens import RefreshToken
+import jwt
+import datetime
+from django.conf import settings
 
 def get_tokens_for_user(user):
-    refresh = RefreshToken.for_user(user)
+    from rest_framework_simplejwt.settings import api_settings
+
+    now = datetime.datetime.utcnow()
+
+    access_payload = {
+        "token_type": "access",
+        "exp": now + api_settings.ACCESS_TOKEN_LIFETIME,
+        "iat": now,
+        settings.SIMPLE_JWT["USER_ID_CLAIM"]: user.email,
+    }
+
+    refresh_payload = {
+        "token_type": "refresh",
+        "exp": now + api_settings.REFRESH_TOKEN_LIFETIME,
+        "iat": now,
+        settings.SIMPLE_JWT["USER_ID_CLAIM"]: user.email,
+    }
+
+    access_token = jwt.encode(access_payload, settings.SIMPLE_JWT["SIGNING_KEY"], algorithm=settings.SIMPLE_JWT["ALGORITHM"])
+    refresh_token = jwt.encode(refresh_payload, settings.SIMPLE_JWT["SIGNING_KEY"], algorithm=settings.SIMPLE_JWT["ALGORITHM"])
+
     return {
-        'refresh': str(refresh),
-        'access': str(refresh.access_token),
+        "access": access_token,
+        "refresh": refresh_token
     }

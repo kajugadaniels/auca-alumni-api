a5df25b1d82dfaa95f48baee2d842d0668337571
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-06-16 14:04:46 +0200

    Implement RegisterUserView with student ID verification and user creation

--- account/views.py
+++ account/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from .serializers import *
+from rest_framework import status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+class RegisterUserView(APIView):
+    def post(self, request):
+        serializer = RegisterUserSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "User registered successfully."}, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

377d8a71ed330c0ad9df2278a97f695e94a31551
KAJUGA Daniels <kajugadaniels@gmail.com>
2025-06-16 14:57:13 +0200

    Define custom unmanaged User model using existing users table

--- account/models.py
+++ account/models.py
@@ -2,16 +2,19 @@ from django.db import models
 
 class User(models.Model):
     id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=255)
     email = models.EmailField(unique=True)
+    email_verified_at = models.DateTimeField(null=True)
     password = models.CharField(max_length=255)
-    student_id = models.IntegerField()
-    type = models.CharField(max_length=255)
-    is_staff = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
+    remember_token = models.CharField(max_length=100, null=True)
     created_at = models.DateTimeField(null=True)
     updated_at = models.DateTimeField(null=True)
+    first_name = models.CharField(max_length=255, null=True)
+    last_name = models.CharField(max_length=255, null=True)
+    phone_number = models.CharField(max_length=255, null=True)
+    is_staff = models.BooleanField(null=True)
+    student_id = models.IntegerField()
+    type = models.CharField(max_length=255, null=True)
 
     class Meta:
         db_table = 'users'
-        managed = False  # Prevent Django from creating/migrating the table
+        managed = False
